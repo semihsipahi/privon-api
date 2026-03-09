898d10f0916ad925961bae90c6f02da002d9859d
yigitkoltuk <yigitkoltuk13@gmail.com>
2026-03-09 23:42:01 +0300

    feat: Register Restaurant model in Reservation module and update environment variables for database connection and beta mode.

--- src/modules/reservation/reservation.module.ts
+++ src/modules/reservation/reservation.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Reservation, ReservationSchema } from '../../models/reservation.schema';
 import { Slot, SlotSchema } from '../../models/slot.schema';
 import { User, UserSchema } from '../../models/user.schema';
+import { Restaurant, RestaurantSchema } from '../../models/restaurant.schema';
 import { DelayedNotificationJob, DelayedNotificationJobSchema } from '../../models/delayed-notification-job.schema';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
@@ -15,6 +16,7 @@ import { NotificationModule } from '../notification/notification.module';
             { name: Reservation.name, schema: ReservationSchema },
             { name: Slot.name, schema: SlotSchema },
             { name: User.name, schema: UserSchema },
+            { name: Restaurant.name, schema: RestaurantSchema },
             { name: DelayedNotificationJob.name, schema: DelayedNotificationJobSchema }
         ]),
         MailModule,
